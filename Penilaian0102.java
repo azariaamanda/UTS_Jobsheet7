e86c55a4509ebc72fea56b580a2b5a15f99843c5
Arsya-Fisa <Arsya Fikri S>
2025-04-17 10:05:15 +0700

    UTS

--- Penilaian0102.java
+++ Penilaian0102.java
@@ -5,5 +5,19 @@ public class Penilaian0102 {
     public double nilaiUAS;
     public double nilaiAkhir;
 
-    public P
+    public Penilaian0102(double nilaiTugas, double nilaiUTS, double nilaiUAS){
+        this.nilaiTugas = nilaiTugas;
+        this.nilaiUTS = nilaiUTS;
+        this.nilaiUAS = nilaiUAS;
+    }
+    public void hitungNiliaiAkhir(){
+        this.nilaiAkhir = (0.3 * this.nilaiTugas) + (0.3 * this.nilaiUTS) + (0.4 * this.nilaiUAS);
+    }
+    public void tampil(){
+        System.out.println("Nilai Tugas: "+ this.nilaiTugas);
+        System.out.println("Nilai UTS: "+ this.nilaiUTS);
+        System.out.println("Nilai UAS: "+this.nilaiUAS);
+        System.out.println("Nilai Akhir: " +this.nilaiAkhir);
+    }    
 }
+
